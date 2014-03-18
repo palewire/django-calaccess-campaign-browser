b56f33c16eaf866ac99819a0c635122619bc0400
Aaron Williams <aaron.colby.williams@gmail.com>
2014-03-18 11:37:42 -0700

    moved the API urls to campaign finance to keep scope clean

--- campaign_finance/urls.py
+++ campaign_finance/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
+from tastypie.api import Api
+
+from campaign_finance.api import FilerResource
 from campaign_finance import views
 
+v1_api = Api(api_name='v1')
+v1_api.register(FilerResource())
 
 urlpatterns = patterns('campaign_finance.views',
 
@@ -9,4 +14,8 @@ urlpatterns = patterns('campaign_finance.views',
     url(r'^filer/(?P<pk>\d+)/$', views.FilerDetailView.as_view(template_name='filer/detail.html'), name='filer_detail'),
     url(r'^committee/(?P<pk>\d+)/$', views.CommitteeDetailView.as_view(template_name='committee/detail.html'), name='committee_detail'),
     url(r'^filing/(?P<pk>\d+)/$', views.FilingDetailView.as_view(template_name='filing/detail.html'), name='filing_detail'),
+
+    # API
+    url(r'^api/', include(v1_api.urls)),
+
 )
