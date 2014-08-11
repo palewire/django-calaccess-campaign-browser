383c8e212a28438f80bc4d85726e61475221edff
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-11 11:38:03 -0700

    PEP8 formatting :sob:

--- campaign_finance/api.py
+++ campaign_finance/api.py
@@ -2,6 +2,7 @@ from tastypie.resources import ModelResource
 from .models import Filer, Committee, Filing
 from .utils.serializer import CIRCustomSerializer
 
+
 class FilerResource(ModelResource):
     class Meta:
         queryset = Filer.objects.all()
