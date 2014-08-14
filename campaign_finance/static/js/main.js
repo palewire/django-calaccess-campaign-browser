e74440aa55d0dd80c84f211ee4c12b8cc98e9ffd
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-14 12:51:04 -0700

    remove unused js

--- campaign_finance/static/js/main.js
+++ campaign_finance/static/js/main.js
@@ -4,44 +4,7 @@ var App = App || {};
 
 App = {
     init: function () {
-        App._determineRangeSize(App.dataTables)
-    },
-    createCalendar: function (selector, data) {
-        var cal = new CalHeatMap();
-        cal.init({
-            // passed in values
-            itemSelector: selector,
-            start: data.startDate(),
-            data: data.data,
-
-            // formatting
-            domain: 'year',
-            subDomain: 'x_day',
-            //subDomainTextFormat: "%d",
-            tooltip: true,
-            domainMargin: [0, 10, 0, 0],
-            legend: [100, 1000, 10000, 10000],
-            range: App._determineRangeSize(),
-            previousSelector: '#previous',
-            nextSelector: '#next',
-        });
-    },
-    clearCalendar: function (selector) {
-        // This is busted kind
-        var elements = document.querySelector(selector).children;
-
-        _.each(elements, function (el) {
-            el.remove();
-        });
-    },
-    _determineRangeSize: function (callback) {
-        var browserWidth = document.documentElement.clientWidth;
-
-        if (browserWidth <= 420) {
-            // Mobile
-        } else {
-            callback();
-        }
+        App.dataTables();
     },
     dataTables: function () {
         $('table').dataTable();
