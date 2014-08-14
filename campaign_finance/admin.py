7e8039303e30639a7d4af8f5a8cc02545c0aaf33
Aaron Williams <aaron.colby.williams@gmail.com>
2014-08-14 10:36:10 -0700

    added admin stuff

--- campaign_finance/admin.py
+++ campaign_finance/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from campaign_finance.models import Committee, Filer
+from campaign_finance.models import Committee, Filer, Summary, Expenditure, Contribution
 
 admin.site.register(Committee)
 admin.site.register(Filer)
+admin.site.register(Expenditure)
+admin.site.register(Contribution)
