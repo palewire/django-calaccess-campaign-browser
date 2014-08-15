6b3df97f656f218fcc87b47195cb6baf2ab67049
Anthony Pesce <anthonyjpesce@gmail.com>
2014-08-15 13:45:40 -0700

    maybe going back to the mixins

--- campaign_finance/views.py
+++ campaign_finance/views.py
@@ -3,13 +3,44 @@ import csv
 import json
 from django.views import generic
 from django.db.models import Sum, Count
+from django.utils.encoding import smart_text
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse
 from campaign_finance.models import Filer, Committee, Filing, Expenditure, Contribution
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse
 
-from django.utils.encoding import smart_text
+#
+# Mixins
+#
 
+class JSONResponseMixin(object):
+    """
+    A mixin that can be used to render a JSON response.
+    """
+    def render_to_json_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return JsonResponse(
+            self.get_data(context),
+            **response_kwargs
+        )
+
+    def get_data(self, context):
+        """
+        Returns an object that will be serialized as JSON by json.dumps().
+        """
+        # Note: This is *EXTREMELY* naive; in reality, you'll need
+        # to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets
+        # -- can be serialized as JSON.
+        return context
+
+
+
+#
+# Views
+#
 
 class IndexView(generic.ListView):
     model = Filer
