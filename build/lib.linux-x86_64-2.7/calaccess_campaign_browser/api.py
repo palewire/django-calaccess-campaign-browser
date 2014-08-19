09a6516aea55538d09be58a5eb8a68928c42de71
palewire <ben.welsh@gmail.com>
2014-08-18 22:14:32 -0700

    This other build dir shouldn't be in the repo either.

--- build/lib.linux-x86_64-2.7/calaccess_campaign_browser/api.py
+++
@@ -1,9 +0,0 @@
-from tastypie.resources import ModelResource
-from .models import Filer
-from .utils.serializer import CIRCustomSerializer
-
-
-class FilerResource(ModelResource):
-    class Meta:
-        queryset = Filer.objects.all()
-        serializer = CIRCustomSerializer()
