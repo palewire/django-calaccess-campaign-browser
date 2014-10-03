c808b96385b5b82a84ba2e566e21bde55632358e
palewire <ben.welsh@gmail.com>
2014-10-03 10:04:26 -0700

    Docs

--- calaccess_campaign_browser/management/commands/dropcalaccesscampaignbrowser.py
+++ calaccess_campaign_browser/management/commands/dropcalaccesscampaignbrowser.py
@@ -4,10 +4,16 @@ from calaccess_campaign_browser.management.commands import CalAccessCommand
 
 
 class Command(CalAccessCommand):
+    help = "Drops all CAL-ACCESS campaign browser database tables"
 
     def handle(self, *args, **options):
+        self.header("Dropping CAL-ACCESS campaign browser database tables")
         self.cursor = connection.cursor()
-        self.header("Dropping CAL-ACCESS campaign browser tables")
+
+        # Ignore MySQL "note" warnings so this can be run with DEBUG=True
+        self.cursor.execute("""SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0;""")
+
+        # Loop through the models and drop all the tables
         model_list = [
             models.Contribution,
             models.Expenditure,
@@ -18,9 +24,10 @@ class Command(CalAccessCommand):
             models.Committee,
             models.Filer,
         ]
-        self.cursor.execute("""SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0;""")
         sql = """DROP TABLE IF EXISTS `%s`;"""
         for m in model_list:
             self.log(" %s" % m.__name__)
             self.cursor.execute(sql % m._meta.db_table)
+
+        # Revert database to default "note" warning behavior
         self.cursor.execute("""SET SQL_NOTES=@OLD_SQL_NOTES;""")
