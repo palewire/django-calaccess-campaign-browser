e68a368c6ddc14093df21701852ecf3f7fa167eb
palewire <ben.welsh@gmail.com>
2015-01-08 10:32:40 -0800

    Added PyMySQL trick to manage.py

--- example/manage.py
+++ example/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     sys.path.append(os.path.dirname(os.path.dirname(__file__)))
