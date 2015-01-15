8b8cd55748bea2b2ea8b72b7a06b47133da45d77
Francis Tseng <f+accounts@frnsys.com>
2015-01-15 11:30:52 -0800

    begun scraper refactor

--- calaccess_campaign_browser/management/commands/scrapeelectioncandidates.py
+++ calaccess_campaign_browser/management/commands/scrapeelectioncandidates.py
@@ -1,175 +1,114 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-# Scraper imports
 import re
 from time import sleep
 
-import requests
-from bs4 import BeautifulSoup
-from requests.exceptions import HTTPError
 from calaccess_campaign_browser.models import Election, Office, Candidate, Filer
-
+from .utils import parse_election_name, parse_office_name
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        """
-        Scraper to get the list of candidates per election.
-        """
-        election_pattern = re.compile('^.*electNav=(\d+)')
-        url = 'http://cal-access.ss.ca.gov/Campaign/Candidates/list.aspx?view=certified'
+    """
+    Scraper to get the list of candidates per election.
+    """
 
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text)
-            links = soup.findAll('a', href=re.compile(r'^.*&electNav=\d+'))
-            elections = {}
+    def build_results(self):
+        results = {}
 
-            # Skip the first link, it is just "PRIOR ELECTIONS".
-            print('Scraping...')
-            links = links[1:]
-            num_elections = len(links)
-            for idx, link in enumerate(links):
-                m = re.match(election_pattern, link['href'])
-                if not m:
-                    raise CommandError
+        soup = self.make_request('Campaign/Candidates/list.aspx?view=certified')
 
-                election_id = m.group(1)
-                title = link.find_next_sibling('span').text.strip()
+        # Skip the first link, it is just "PRIOR ELECTIONS".
+        links = soup.findAll('a', href=re.compile(r'^.*&electNav=\d+'))
+        links = links[1:]
 
-                try:
-                    elections[title] = self.scrape_election_page(link["href"], idx, num_elections)
+        print('Scraping...')
+        num_elections = len(links)
+        for idx, link in enumerate(links):
+            title = link.find_next_sibling('span').text.strip()
+            results[title] = self.scrape_election_page(link["href"], idx, num_elections)
+            sleep(.5)
 
-                # Try, try again
-                except HTTPError:
-                    print('Got non-200 response, trying again...')
-                    sleep(2.)
-                    elections[title] = self.scrape_election_page(link["href"], idx, num_elections)
+        return results
 
-                sleep(.5)
+    def process_results(self, results):
+        print('Creating and/or updating models...')
+        print('Found %s elections.' % len(results.keys()))
+        for name, election_dict in results.items():
+            print('Election: %s' % name)
+            election_id = election_dict['id']
 
-            print('Creating and/or updating models...')
-            print('Found %s elections.' % len(elections.keys()))
-            for name, election_dict in elections.items():
-                print('Election: %s' % name)
-                election_id = election_dict['id']
+            year = int(name[:4])
+            type = Election.parse_name(name)
 
-                year = int(name[:4])
+            election, created = Election.objects.get_or_create(
+                year=year,
+                name=type,
+                id_raw=election_id,
+                sort_index=election_dict['index'])
 
-                if 'PRIMARY' in name:
-                    type = 'PRIMARY'
-                elif 'GENERAL' in name:
-                    type = 'GENERAL'
-                elif 'SPECIAL RUNOFF' in name:
-                    type = 'SPECIAL_RUNOFF'
-                elif 'SPECIAL' in name:
-                    type = 'SPECIAL'
-                elif 'RECALL' in name:
-                    type = 'RECALL'
-                else:
-                    type = 'OTHER'
+            if created:
+                print('\tCreated %s' % election)
+            else:
+                print('\tGot %s' % election)
 
-                election, created = Election.objects.get_or_create(
-                    year=year,
-                    name=type,
-                    id_raw=election_id,
-                    sort_index=election_dict['index'])
+            for _, office_dict in election_dict['data'].items():
+                for office_name, candidates in office_dict.items():
+                    office_type, seat = parse_office_name(office_name)
 
-                if created:
-                    print('\tCreated %s' % election)
-                else:
-                    print('\tGot %s' % election)
+                    office, created = Office.objects.get_or_create(name=office_type, seat=seat)
 
-                for _, office_dict in election_dict['data'].items():
-                    for office_name, candidates in office_dict.items():
-                        seat = None
-                        if 'LIEUTENANT GOVERNOR' in office_name:
-                            office_type = 'LIEUTENANT_GOVERNOR'
-                        elif 'GOVERNOR' in office_name:
-                            office_type = 'GOVERNOR'
-                        elif 'SECRETARY OF STATE' in office_name:
-                            office_type = 'SECRETARY_OF_STATE'
-                        elif 'CONTROLLER' in office_name:
-                            office_type = 'CONTROLLER'
-                        elif 'TREASURER' in office_name:
-                            office_type = 'TREASURER'
-                        elif 'ATTORNEY GENERAL' in office_name:
-                            office_type = 'ATTORNEY_GENERAL'
-                        elif 'SUPERINTENDENT OF PUBLIC INSTRUCTION' in office_name:
-                            office_type = 'SUPERINTENDENT_OF_PUBLIC_INSTRUCTION'
-                        elif 'INSURANCE COMMISSIONER' in office_name:
-                            office_type = 'INSURANCE_COMMISSIONER'
-                        elif 'MEMBER BOARD OF EQUALIZATION' in office_name:
-                            office_type = 'BOARD_OF_EQUALIZATION'
-                            seat = office_name.split()[-1]
-                        elif 'STATE SENATE' in office_name:
-                            office_type = 'SENATE'
-                            seat = office_name.split()[-1]
-                        elif 'ASSEMBLY' in office_name:
-                            office_type = 'ASSEMBLY'
-                            seat = office_name.split()[-1]
-                        else:
-                            office_type = 'OTHER'
+                    for candidate in candidates:
+                        if not candidate['id']:
+                            continue
 
-                        office, created = Office.objects.get_or_create(name=office_type, seat=seat)
-
-                        for candidate in candidates:
-                            if not candidate['id']:
-                                continue
-
-                            try:
-                                filer = Filer.objects.get(filer_id_raw=int(candidate['id']))
-                                candidate, created = Candidate.objects.get_or_create(election=election, office=office, filer=filer)
-                            except Filer.DoesNotExist:
-                                pass
+                        try:
+                            filer = Filer.objects.get(filer_id_raw=int(candidate['id']))
+                            candidate, created = Candidate.objects.get_or_create(election=election, office=office, filer=filer)
+                        except Filer.DoesNotExist:
+                            pass
 
     def scrape_election_page(self, rel_url, idx, total_elections):
-        url = 'http://cal-access.ss.ca.gov'+rel_url
-        print('Scraping from %s' % url)
-        response = requests.get(url)
-        election_id = url.replace('http://cal-access.ss.ca.gov/Campaign/Candidates/list.aspx?view=certified&electNav=', '')
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text)
-            sections = {}
-            for section in soup.findAll('a', {'name': re.compile(r'[a-z]+')}):
+        print('Scraping from %s' % rel_url)
+        sections = {}
+        election_id = re.match(r'.+electNav=(\d+)', rel_url).group(1)
+
+        soup = self.make_request(rel_url)
+        for section in soup.findAll('a', {'name': re.compile(r'[a-z]+')}):
+            # Check that this data matches the structure we expect.
+            section_name_el = section.find('span', {'class': 'hdr14'})
+            if not section_name_el:
+                continue
+            section_name = section_name_el.text
+            sections[section_name] = {}
+
+            offices = section.findAll('td')
+            for office in offices:
                 # Check that this data matches the structure we expect.
-                section_name_el = section.find('span', {'class': 'hdr14'})
-                if not section_name_el:
+                title_el = office.find('span', {'class': 'hdr13'})
+                if not title_el:
                     continue
-                section_name = section_name_el.text
-                sections[section_name] = {}
+                title = title_el.text
 
-                offices = section.findAll('td')
-                for office in offices:
-                    # Check that this data matches the structure we expect.
-                    title_el = office.find('span', {'class': 'hdr13'})
-                    if not title_el:
-                        continue
-                    title = title_el.text
+                people = []
+                for p in office.findAll('a', {'class': 'sublink2'}):
+                    people.append({
+                        'name': p.text,
+                        'id': re.match(r'.+id=(\d+)', p['href']).group(1)
+                    })
+                for p in office.findAll('span', {'class': 'txt7'}):
+                    people.append({
+                        'name': p.text,
+                        'id':  None
+                    })
 
-                    people = []
-                    for p in office.findAll('a', {'class': 'sublink2'}):
-                        people.append({
-                            'name': p.text,
-                            'id':  p['href'].replace('/Campaign/Candidates/Detail.aspx?id=', '')
-                        })
-                    for p in office.findAll('span', {'class': 'txt7'}):
-                        people.append({
-                            'name': p.text,
-                            'id':  None
-                        })
+                sections[section_name][title] = people
 
-                    sections[section_name][title] = people
-
-            # The index value is used to preserve sorting of elections,
-            # since multiple elections may occur in a year.
-            # BeautifulSoup goes from top to bottom,
-            # but the top most election is the most recent so it should
-            # have the highest id.
-            return {
-                'id': int(election_id),
-                'data': sections,
-                'index': total_elections - idx
-            }
-
-        else:
-            raise HTTPError
+        # The index value is used to preserve sorting of elections,
+        # since multiple elections may occur in a year.
+        # BeautifulSoup goes from top to bottom,
+        # but the top most election is the most recent so it should
+        # have the highest id.
+        return {
+            'id': int(election_id),
+            'data': sections,
+            'index': total_elections - idx
+        }
