6d14ed9c6f4eb08d1cd87c55508e96d67a0f040f
palewire <ben.welsh@gmail.com>
2015-02-15 17:38:23 -0800

    Just call it object

--- calaccess_campaign_browser/views/committees.py
+++ calaccess_campaign_browser/views/committees.py
@@ -19,7 +19,6 @@ class CommitteeDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(CommitteeDetailView, self).get_context_data(**kwargs)
-        context['committee'] = self.object
 
         context['committee_json'] = SafeString(
             json.dumps(model_to_dict(self.object), cls=LazyEncoder)
