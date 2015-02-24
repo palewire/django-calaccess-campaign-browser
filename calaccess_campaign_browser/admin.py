b7535540335186c491b24bc6526e98235d886332
palewire <ben.welsh@gmail.com>
2015-02-23 19:27:09 -0800

    Removed add and delete permissions from the base admin

--- calaccess_campaign_browser/admin.py
+++ calaccess_campaign_browser/admin.py
@@ -8,6 +8,12 @@ class BaseAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, *args, **kwargs):
         return [f.name for f in self.model._meta.fields]
 
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(models.Filing)
 class FilingAdmin(BaseAdmin):
