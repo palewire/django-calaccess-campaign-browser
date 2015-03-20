379ab7cdd488d710636811ab25a6c67e60d0f94c
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-19 17:48:39 -0700

    Rewrote Expenditure model, manage and load command. For #152

--- calaccess_campaign_browser/management/commands/loadcalaccesscampaignexpenditures.py
+++ calaccess_campaign_browser/management/commands/loadcalaccesscampaignexpenditures.py
@@ -1,135 +1,218 @@
+import os
+import csv
+import copy
+import MySQLdb
+import warnings
+import tempfile
 from django.db import connection
+from calaccess_raw.models import ExpnCd, S496Cd, S498Cd
+from calaccess_raw import get_download_directory
 from calaccess_campaign_browser.management.commands import CalAccessCommand
+from calaccess_campaign_browser.models import Expenditure, Filing, Committee
 
+from ipdb import set_trace as debugger
 
 class Command(CalAccessCommand):
     help = "Load refined campaign expenditures from CAL-ACCESS raw data"
 
+    def set_options(self, *args, **kwargs):
+        self.data_dir = os.path.join(get_download_directory(), 'csv')
+        self.cursor = connection.cursor()
+        # Quarterlies stuff
+        self.quarterly_tmp_csv = tempfile.NamedTemporaryFile().name
+        self.quarterly_target_csv = os.path.join(
+            self.data_dir,
+            'expn_cd_transformed.csv'
+        )
+        # Late filings stuff
+        self.late_tmp_csv = tempfile.NamedTemporaryFile().name
+        self.late_target_csv = os.path.join(
+            self.data_dir,
+            's496_cd_transformed.csv'
+        )
+        self.late_tmp_table = "TMP_%s" % S496Cd._meta.db_table
+
+    def transform_quarterly_expenditures_csv(self):
+        self.log("  Marking duplicates")
+        self.log("   Dumping CSV sorted by unique identifier")
+        sql = """
+        SELECT
+            `agent_namf`,
+            `agent_naml`,
+            `agent_nams`,
+            `agent_namt`,
+            `amend_id`,
+            `amount`,
+            `bakref_tid`,
+            `bal_juris`,
+            `bal_name`,
+            `bal_num`,
+            `cand_namf`,
+            `cand_naml`,
+            `cand_nams`,
+            `cand_namt`,
+            `cmte_id`,
+            `cum_oth`,
+            `cum_ytd`,
+            `dist_no`,
+            `entity_cd`,
+            `expn_chkno`,
+            `expn_code`,
+            `expn_date`,
+            `expn_dscr`,
+            `filing_id`,
+            `form_type`,
+            `g_from_e_f`,
+            `juris_cd`,
+            `juris_dscr`,
+            `line_item`,
+            `memo_code`,
+            `memo_refno`,
+            `off_s_h_cd`,
+            `offic_dscr`,
+            `office_cd`,
+            `payee_adr1`,
+            `payee_adr2`,
+            `payee_city`,
+            `payee_namf`,
+            `payee_naml`,
+            `payee_nams`,
+            `payee_namt`,
+            `payee_st`,
+            `payee_zip4`,
+            `rec_type`,
+            `sup_opp_cd`,
+            `tran_id`,
+            `tres_adr1`,
+            `tres_adr2`,
+            `tres_city`,
+            `tres_namf`,
+            `tres_naml`,
+            `tres_nams`,
+            `tres_namt`,
+            `tres_st`,
+            `tres_zip4`,
+            `xref_match`,
+            `xref_schnm`
+        FROM %(raw_model)s
+        ORDER BY filing_id, tran_id, amend_id DESC
+        INTO OUTFILE '%(tmp_csv)s'
+        FIELDS TERMINATED BY ','
+        ENCLOSED BY '"'
+        LINES TERMINATED BY '\n'
+        """ % dict(
+            raw_model=ExpnCd._meta.db_table,
+            tmp_csv=self.quarterly_tmp_csv,
+        )
+        self.cursor.execute(sql)
+
+        INHEADERS = [
+            "agent_namf",
+            "agent_naml",
+            "agent_nams",
+            "agent_namt",
+            "amend_id",
+            "amount",
+            "bakref_tid",
+            "bal_juris",
+            "bal_name",
+            "bal_num",
+            "cand_namf",
+            "cand_naml",
+            "cand_nams",
+            "cand_namt",
+            "cmte_id",
+            "cum_oth",
+            "cum_ytd",
+            "dist_no",
+            "entity_cd",
+            "expn_chkno",
+            "expn_code",
+            "expn_date",
+            "expn_dscr",
+            "filing_id",
+            "form_type",
+            "g_from_e_f",
+            "juris_cd",
+            "juris_dscr",
+            "line_item",
+            "memo_code",
+            "memo_refno",
+            "off_s_h_cd",
+            "offic_dscr",
+            "office_cd",
+            "payee_adr1",
+            "payee_adr2",
+            "payee_city",
+            "payee_namf",
+            "payee_naml",
+            "payee_nams",
+            "payee_namt",
+            "payee_st",
+            "payee_zip4",
+            "rec_type",
+            "sup_opp_cd",
+            "tran_id",
+            "tres_adr1",
+            "tres_adr2",
+            "tres_city",
+            "tres_namf",
+            "tres_naml",
+            "tres_nams",
+            "tres_namt",
+            "tres_st",
+            "tres_zip4",
+            "xref_match",
+            "xref_schnm"
+        ]
+        OUTHEADERS = copy.copy(INHEADERS)
+        OUTHEADERS.append("IS_DUPLICATE")
+
+        self.log("   Marking duplicates in a new CSV")
+        # `rU` is read Universal
+        # see: https://docs.python.org/2/library/functions.html#open
+        with open(self.quarterly_tmp_csv, 'rU') as fin:
+            fout = csv.DictWriter(
+                open(self.quarterly_target_csv, 'wb'),
+                fieldnames=OUTHEADERS
+            )
+            fout.writeheader()
+            last_uid = ''
+
+            reader = csv.DictReader(fin, fieldnames=INHEADERS)
+
+            for row in reader:
+                row.pop(None, None)
+                uid = '{}-{}'.format(
+                    row['filing_id'],
+                    row['tran_id']
+                )
+
+                if uid != last_uid:
+                    row['is_duplicate'] = 0
+                    last_uid = uid
+                else:
+                    row['is_duplicate'] = 1
+
+                try:
+                    fout.writerow(row)
+
+                except ValueError, e:
+                    continue
+
+    def load_quarterly_expenditures(self):
+        pass
+
     def handle(self, *args, **options):
         self.header("Loading expenditures")
-        c = connection.cursor()
-        sql = """
-        INSERT INTO calaccess_campaign_browser_expenditure (
-            cycle_id,
-            committee_id,
-            filing_id,
-            dupe,
-            line_item,
-            payee_namt,
-            payee_namf,
-            payee_naml,
-            payee_nams,
-            expn_dscr,
-            payee_zip4,
-            g_from_e_f,
-            payee_city,
-            amount,
-            memo_refno,
-            expn_code,
-            memo_code,
-            entity_cd,
-            bakref_tid,
-            payee_adr1,
-            payee_adr2,
-            expn_chkno,
-            form_type,
-            cmte_id,
-            xref_schnm,
-            xref_match,
-            expn_date,
-            cum_ytd,
-            payee_st,
-            tran_id,
-            name,
-            person_flag,
-            raw_org_name
-        )
-        SELECT
-            f.cycle_id as cycle_id,
-            f.committee_id as committee_id,
-            f.id as filing_id,
-            f.is_duplicate,
-            e.line_item,
-            e.payee_namt,
-            e.payee_namf,
-            e.payee_naml,
-            e.payee_nams,
-            e.expn_dscr,
-            e.payee_zip4,
-            e.g_from_e_f,
-            e.payee_city,
-            e.amount,
-            e.memo_refno,
-            e.expn_code,
-            e.memo_code,
-            e.entity_cd,
-            e.bakref_tid,
-            e.payee_adr1,
-            e.payee_adr2,
-            e.expn_chkno,
-            e.form_type,
-            e.cmte_id,
-            e.xref_schnm,
-            e.xref_match,
-            e.expn_date,
-            e.cum_ytd,
-            e.payee_st,
-            e.tran_id,
-            TRIM(
-                CASE
-                    WHEN e.payee_naml = '' THEN
-                        REPLACE(CONCAT(
-                            e.bal_name,
-                            " ",
-                            REPLACE(TRIM(
-                                CONCAT(
-                                    e.`cand_namt`,
-                                    " ",
-                                    e.`cand_namf`,
-                                    " ",
-                                    e.`cand_naml`,
-                                    " ",
-                                    e.`cand_nams`
-                                )
-                            ), '  ', ' '),
-                            " ",
-                            e.juris_dscr,
-                            " ",
-                            e.offic_dscr
-                        ), '  ', ' ')
-                    ELSE
-                        REPLACE(TRIM(
-                            CONCAT(
-                                e.`payee_namt`,
-                                " ",
-                                e.`payee_namf`,
-                                " ",
-                                e.`payee_naml`,
-                                " ",
-                                e.`payee_nams`
-                            )
-                        ), '  ', ' ')
-                END
-            ) as `name`,
-            CASE
-                WHEN e.payee_naml = '' THEN
-                    false
-                WHEN e.payee_naml <> '' AND e.payee_namf = '' THEN
-                    false
-                ELSE
-                    true
-            END as `person_flag`,
-            CASE
-                WHEN e.payee_naml = '' THEN
-                    ''
-                WHEN e.payee_naml <> '' AND e.payee_namf = '' THEN
-                    e.payee_naml
-                ELSE
-                    ''
-            END as `raw_org_name`
-        FROM calaccess_campaign_browser_filing as f
-        INNER JOIN EXPN_CD as e
-        ON f.filing_id_raw = e.filing_id
-        AND f.amend_id = e.amend_id
-        """
-        c.execute(sql)
+        self.set_options(*args, **options)
+
+        warnings.filterwarnings("ignore", category=MySQLdb.Warning)
+
+        self.log(" Quarterly filings")
+        self.transform_quarterly_expenditures_csv()
+        self.load_quarterly_expenditures()
+
+        # self.log(" Late filings")
+        # self.transform_late_expenditures_csv()
+        # self.load_late_expenditures()
--- calaccess_campaign_browser/models/expenditures.py
+++ calaccess_campaign_browser/models/expenditures.py
@@ -7,6 +7,51 @@ class Expenditure(BaseModel):
     """
     Who got paid and how much.
     """
+    cycle = models.ForeignKey('Cycle')
+    committee = models.ForeignKey(
+        'Committee',
+        related_name='expenditure_to'
+    )
+    filing = models.ForeignKey('Filing')
+
+    # CAL-ACCESS ids
+    filing_id_raw = models.IntegerField(db_index=True, null=True)
+    transaction_id = models.CharField(
+        'transaction ID',
+        max_length=20,
+        db_index=True,
+        blank=True,
+        null=True
+    )
+    amend_id = models.IntegerField('amendment', db_index=True)
+    backreference_transaction_id = models.CharField(
+        'backreference transaction ID',
+        max_length=50,
+        db_index=True
+    )
+    is_crossreference = models.CharField(max_length=1, blank=True)
+    crossreference_schedule = models.CharField(max_length=2, blank=True)
+
+    # Basics about expenditure
+    is_duplicate = models.BooleanField(default=False)
+
+    date_received = models.DateField(null=True)
+    expenditure_description = models.CharField(max_length=255, blank=True)
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
+
+    # About the candidate
+    candidate_full_name = models.CharField(max_length=255)
+    candidate_is_person = models.BooleanField(default=False)
+    candidate_committee = models.ForeignKey(
+        'Committee',
+        null=True,
+        related_name="expenditures_from"
+    )
+    candidate_prefix = models.CharField(max_length=10, blank=True)
+    candidate_first_name = models.CharField(max_length=255, blank=True)
+    candidate_last_name = models.CharField(max_length=200, blank=True)
+    candidate_suffix = models.CharField(max_length=10, blank=True)
+
     EXPENDITURE_CODE_CHOICES = (
         ('CMP', 'campaign paraphernalia/misc.'),
         ('CNS', 'campaign consultants'),
@@ -37,54 +82,44 @@ class Expenditure(BaseModel):
         ('VOT', 'voter registration'),
         ('WEB', 'information technology costs (internet, e-mail)'),
     )
-    cycle = models.ForeignKey('Cycle')
-    committee = models.ForeignKey('Committee')
-    filing = models.ForeignKey('Filing')
-
-    # Raw data fields
-    amount = models.DecimalField(max_digits=16, decimal_places=2)
-    bakref_tid = models.CharField(max_length=50L, blank=True)
-    cmte_id = models.CharField(max_length=9L, blank=True)
-    cum_ytd = models.DecimalField(max_digits=16, decimal_places=2, null=True)
-    entity_cd = models.CharField(max_length=5L, blank=True)
-    expn_chkno = models.CharField(max_length=20L, blank=True)
-    expn_code = models.CharField(
-        max_length=3L,
+    candidate_expense_code = models.CharField(
+        max_length=3,
         blank=True,
         choices=EXPENDITURE_CODE_CHOICES
     )
-    expn_date = models.DateField(null=True)
-    expn_dscr = models.CharField(max_length=400L, blank=True)
-    form_type = models.CharField(max_length=6L, blank=True)
-    # back reference from Form 460 Schedule G to Schedule E or F
-    g_from_e_f = models.CharField(max_length=1L, blank=True)
-    line_item = models.IntegerField()
-    memo_code = models.CharField(max_length=1L, blank=True)
-    memo_refno = models.CharField(max_length=20L, blank=True)
-    payee_adr1 = models.CharField(max_length=55L, blank=True)
-    payee_adr2 = models.CharField(max_length=55L, blank=True)
-    payee_city = models.CharField(max_length=30L, blank=True)
-    payee_namf = models.CharField(max_length=255L, blank=True)
-    payee_naml = models.CharField(max_length=200L, blank=True)
-    payee_nams = models.CharField(max_length=10L, blank=True)
-    payee_namt = models.CharField(max_length=10L, blank=True)
-    payee_st = models.CharField(max_length=2L, blank=True)
-    payee_zip4 = models.CharField(max_length=10L, blank=True)
-    tran_id = models.CharField(max_length=20L, blank=True)
-    # a related item on other schedule has the same transaction identifier.
-    # "X" indicates this condition is true
-    xref_match = models.CharField(max_length=1L, blank=True)
-    # Related record is included on Form 460 Schedules B2 or F
-    xref_schnm = models.CharField(max_length=2L, blank=True)
+    ENTITY_CD_CHOICES = (
+        ('COM', 'Recipient Committee'),
+        ('RCP', 'Recipient Committee'),
+        ('IND', 'Individual'),
+        ('OTH', 'Other'),
+        ('PTY', 'PTY - Unknown'),
+        ('SCC', 'SCC 0 Unknown'),
+        ('BNM', 'BNM - Unknown'),
+        ('CAO', 'CAO - Unknown'),
+        ('OFF', 'OFF - Unknown'),
+        ('PTH', 'PTH - Unknown'),
+        ('RFD', 'RFD - Unknown'),
+        ('MBR', 'MBR - Unknown'),
+        ('0', '0 - Unknown'),
+    )
+    candidate_entity_type = models.CharField(
+        max_length=3,
+        blank=True,
+        help_text="The type of entity that made that expenditure",
+        choices=ENTITY_CD_CHOICES
+    )
 
-    # Derived fields
-    name = models.CharField(max_length=255)
-    raw_org_name = models.CharField(max_length=255)
-    person_flag = models.BooleanField(default=False)
-    org_id = models.IntegerField(null=True)
-    individual_id = models.IntegerField(null=True)
-
-    dupe = models.BooleanField(default=False)
+    # About the payee
+    payee_prefix = models.CharField(max_length=10, blank=True)
+    payee_first_name = models.CharField(max_length=255, blank=True)
+    payee_last_name = models.CharField(max_length=200, blank=True)
+    payee_suffix = models.CharField(max_length=10, blank=True)
+    payee_address_1 = models.CharField(max_length=55, blank=True)
+    payee_address_2 = models.CharField(max_length=55, blank=True)
+    payee_city = models.CharField(max_length=30, blank=True)
+    payee_state = models.CharField(max_length=2, blank=True)
+    payee_zipcode = models.CharField(max_length=10, blank=True)
+    payee_committee_id = models.CharField(max_length=9, blank=True)
     objects = models.Manager()
     real = managers.RealExpenditureManager()
 
@@ -100,7 +135,23 @@ class Expenditure(BaseModel):
         from calaccess_raw.models import ExpnCd
         return ExpnCd.objects.get(
             amend_id=self.amend_id,
-            filing_id=self.filing_id,
-            tran_id=self.tran_id,
-            bakref_tid=self.bakref_tid
+            filing_id=self.filing.filing_id_raw,
+            tran_id=self.transaction_id,
+            bakref_tid=self.backreference_transaction_id
         )
+
+    @property
+    def candidate_dict(self):
+        d = SortedDict({})
+        for k, v in self.to_dict().items():
+            if k.startswith("candidate"):
+                d[k.replace("candidate ", "")] = v
+        return d
+
+    @property
+    def payee_dict(self):
+        d = SortedDict({})
+        for k, v in self.to_dict().items():
+            if k.startswith("payee"):
+                d[k.replace("payee ", "")] = v
+        return d
