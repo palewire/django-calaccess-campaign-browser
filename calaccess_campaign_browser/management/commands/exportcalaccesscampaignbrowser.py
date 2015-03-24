21b3a87dec1d4754a722087b51be65c7dcc36563
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-23 17:41:57 -0700

    Rewrite for the export command. For #200

--- calaccess_campaign_browser/management/commands/exportcalaccesscampaignbrowser.py
+++ calaccess_campaign_browser/management/commands/exportcalaccesscampaignbrowser.py
@@ -1,17 +1,13 @@
 import os
-import csvkit
+import csv
+import datetime
 from optparse import make_option
-from collections import OrderedDict
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.db.models import get_model
+from calaccess_campaign_browser.management.commands import CalAccessCommand
 
-from calaccess_campaign_browser.models import (
-    Contribution,
-    Cycle,
-    Expenditure,
-    Summary
-)
+from calaccess_campaign_browser.models import Cycle
 
 custom_options = (
     make_option(
@@ -37,122 +33,65 @@ custom_options = (
     ),
 )
 
-contributions_header = OrderedDict([
-    ('amount', 'amount'),
-    ('filing_id', 'filing_id'),
-    ('committee__name', 'committee_name'),
-    ('cycle_id', 'cycle'),
-    ('date_received', 'date_received'),
-    ('contributor_first_name', 'contributor_first_name'),
-    ('contributor_last_name', 'contributor_last_name'),
-    ('contributor_full_name', 'contributor_full_name'),
-    ('contributor_occupation', 'contributor_occupation'),
-    ('contributor_employer', 'contributor_employer'),
-    ('contributor_address_1', 'contributor_address_1'),
-    ('contributor_address_2', 'contributor_address_2'),
-    ('contributor_city', 'contributor_city'),
-    ('contributor_state', 'contributor_state'),
-    ('contributor_zipcode', 'contributor_zipcode'),
-])
-expenditures_header = OrderedDict([
-    ('amount', 'amount'),
-    ('filing_id', 'filing_id'),
-    ('committee__name', 'committee_name'),
-    ('cycle_id', 'cycle'),
-    ('expn_date', 'date_received'),
-    ('payee_namf', 'payee_first_name'),
-    ('payee_naml', 'payee_last_name'),
-    ('name', 'payee_full_name'),
-    ('payee_namt', 'payee_occupation'),
-    ('raw_org_name', 'payee_employer'),
-    ('payee_adr1', 'payee_address_1'),
-    ('payee_adr2', 'payee_address_2'),
-    ('payee_city', 'payee_city'),
-    ('payee_st', 'payee_state'),
-    ('payee_zip4', 'payee_zipcode'),
-])
-summary_header = OrderedDict([
-    # ('committee__filer__name', 'filer'),
-    # ('committee__filer__filer_id', 'filer_id'),
-    # ('committee__name', 'committee'),
-    # ('committee__filer_id_raw', 'committee_id'),
-    # ('cycle__name', 'cycle'),
-    ('ending_cash_balance', 'ending_cash_balance'),
-    ('filing_id_raw', 'filing_id'),
-    ('amend_id', 'amend_id'),
-    # ('filing__start_date', 'filing_start_date'),
-    # ('filing__end_date', 'filing_end_date'),
-    ('itemized_expenditures', 'itemized_expenditures'),
-    (
-        'itemized_monetary_contributions',
-        'itemized_monetary_contributions'
-    ),
-    ('non_monetary_contributions', 'non_monetary_contributions'),
-    ('outstanding_debts', 'outstanding_debts'),
-    ('total_contributions', 'total_contributions'),
-    ('total_expenditures', 'total_expenditures'),
-    ('total_monetary_contributions', 'total_monetary_contributions'),
-    ('unitemized_expenditures', 'unitemized_expenditures'),
-    (
-        'unitemized_monetary_contributions',
-        'unitemized_monetary_contributions'
-    ),
-])
 
-
-class Command(BaseCommand):
+class Command(CalAccessCommand):
     help = 'Export refined CAL-ACCESS campaign browser data as CSV files'
-    option_list = BaseCommand.option_list + custom_options
+    option_list = CalAccessCommand.option_list + custom_options
+
+    # Take a list of lists and make every row utf-8
+    # http://stackoverflow.com/a/17527101/868724
+    encoded = lambda x: [[unicode(s).encode('utf-8') for s in t] for t in x]
 
     def set_options(self, *args, **kwargs):
         self.data_dir = os.path.join(
             settings.BASE_DIR, 'data')
         os.path.exists(self.data_dir) or os.mkdir(self.data_dir)
 
+    def export_to_csv(self, model_name):
+        self.header('Exporting models ...')
+
+        today = datetime.datetime.today()
+        model = get_model('calaccess_campaign_browser', model_name)
+        fieldnames = [f.name for f in model._meta.fields]
+        filename = '{}-{}-{}-{}.csv'.format(
+            today.year,
+            today.month,
+            today.day,
+            model_name.lower()
+        )
+        filepath = os.path.join(self.data_dir, filename)
+
+        self.header('  Exporting {} model ...'.format(model_name.capitalize()))
+
+        with open(filepath, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+            writer.writerow(fieldnames)
+
+            if model_name != 'summary' or model_name != 'Summary':
+                for cycle in Cycle.objects.all():
+                    self.log('    Looking at cycle {} ...'.format(cycle.name))
+                    rows = model.objects.filter(cycle=cycle)\
+                        .exclude(is_duplicate=True).values_list(flat=True)
+
+                    if not rows:
+                        self.failure('      No data for {}'.format(cycle.name))
+
+                    else:
+                        rows = self.encoded(rows)
+                        writer.writerows(rows)
+                        self.success('      Added {} {} data'.format(
+                            cycle.name, model_name))
+
+        self.success('  Exported {}!'.format(model_name.capitalize()))
+
     def handle(self, *args, **options):
         self.set_options(*args, **options)
+
         if options['contributions']:
-            self.export_to_csv('contributions', contributions_header)
+            self.export_to_csv('contribution')
+
         if options['expenditures']:
-            self.export_to_csv('expenditures', expenditures_header)
+            self.export_to_csv('expenditure')
+
         if options['summary']:
-            self.export_to_csv('summary', summary_header)
-
-    def export_to_csv(self, outfile_name, header_translation):
-        print 'working on %s' % outfile_name
-        csv_name = '%s.csv' % outfile_name
-        outfile_path = os.path.join(self.data_dir,  csv_name)
-        outfile = open(outfile_path, 'w')
-
-        csv_writer = csvkit.unicsv.UnicodeCSVDictWriter(
-            outfile, fieldnames=header_translation.keys(), delimiter=',')
-
-        csv_writer.writerow(header_translation)
-
-        for c in Cycle.objects.all():
-            if outfile_name == 'contributions':
-                dict_rows = Contribution.objects.filter(cycle=c).exclude(
-                    is_duplicate=True).values(*header_translation.keys())
-                csv_writer.writerows(dict_rows)
-
-            elif outfile_name == 'expenditures':
-                dict_rows = Expenditure.objects.filter(cycle=c).exclude(
-                    dupe=True).values(*header_translation.keys())
-                csv_writer.writerows(dict_rows)
-
-            elif outfile_name == 'summary':
-                pass
-
-            else:
-                print("You did not specify 'contributions, \
-                    'expenditures' or 'summary'. Exiting")
-                raise
-
-        # Summary specific
-        if outfile_name == 'summary':
-            rows = Summary.objects.all().values(*header_translation.keys())
-            csv_writer.writerows(rows)
-
-        outfile.close()
-
-        print('Exported %s' % outfile_name)
+            self.export_to_csv('summary')
